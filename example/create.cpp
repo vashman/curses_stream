ee081d5a607d4b3982c8e8f7aa6de36dc1b347cd
Sunny S <3030712@gmail.com>
2014-11-09 00:47:58 -0800

    updated create example, with pass buffer

--- example/create.cpp
+++ example/create.cpp
@@ -4,10 +4,16 @@ using curstream::cursesbuf;
 using curstream::curses_stream;
 
 int main(){
+/* new curses stream */
 curses_stream io;
+
 /* same as above, notice how creating a second instance does not start
 another session over the terminal but shares it with the previous.
 */
-curses_stream io2(new cursesbuf());
+curses_stream io2;
+
+/* create stream by passing cursesbuf */
+cursesbuf buf;
+curses_stream io2(buf);
 return 0;
 }
